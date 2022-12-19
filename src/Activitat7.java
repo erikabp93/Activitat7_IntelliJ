454e62d30515ce709a58a58fa356a7072c7bc96a
Erika Ballesteros <erikabalpas22@gmail.com>
2022-12-19 23:30:00 +0100

    Segon commit

--- src/Activitat7.java
+++ src/Activitat7.java
@@ -1,6 +1,6 @@
 public class Activitat7 {
     public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 0; i <= 20; i++) {
             System.out.print(i + " ");
         }
     }
